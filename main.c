946740114a0e91ac239534e94e70d63f38772e33
Pavle <pavle_milanov@hotmail.com>
2022-11-20 01:30:49 +0100

    Some boilerplate code, though it is not working properly.

--- main.c
+++ main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define BOARD_ROWS 6
+#define BOARD_COLS 7
+
+typedef struct Board {
+	int rows;
+	int cols;
+	int (*board)[BOARD_ROWS][BOARD_COLS];
+} Board;
 
 void clear_screen(){
 	#ifdef linux
@@ -23,11 +33,39 @@ void getUserInput(char * s){
 	s[i] = '\0';  // Null terminate the string
 }
 
+void print_board(Board b){
+	printf("|A|B|C|D|E|F|G|");
+	for (int i=0; i<b.cols; i++){
+		printf("\n");
+		for (int j=0; j<b.rows; j++){
+			if (j==0){
+				printf("|%d|", (*b.board)[i][j]);
+			}
+			else{
+				printf("%d|", (*b.board)[i][j]);
+			}
+		}
+	}
+	printf("\n");
+}
+
+Board create_board(int rows, int cols){
+	Board b;
+	b.rows = rows;
+	b.cols = cols;
+	int board[rows][cols];
+	memset(board, 0, sizeof(board));
+	int (*boardPtr)[rows][cols] = &board;
+	b.board = boardPtr;
+	return b;
+}
+
 int main () {
-	int startChoice = 1;
+	int startChoice;
+	Board playingBoard;
 	char *playerOne = malloc(sizeof(char));
 	char *playerTwo = malloc(sizeof(char));
-	clear_screen();
+	// clear_screen();
 	printf("Choose an option:\n");
 	printf("1. Play a new game\n");
 	printf("2. Load saved game\n");
@@ -43,7 +81,9 @@ int main () {
 		printf("Name of the second player:");
 		getUserInput(playerTwo);
 		clear_screen();
-		printf("%s, %s\n", playerOne, playerTwo);
+		playingBoard = create_board(BOARD_ROWS, BOARD_COLS);
+		print_board(playingBoard);
+		// printf("%s, %s\n", playerOne, playerTwo);
 		free(playerOne);
 		free(playerTwo);
 	}
